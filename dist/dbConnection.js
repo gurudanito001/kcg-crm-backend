d11f8d8064e55af9e8de396ac52b0bfc8696b6ea
Daniel Nwokocha <gurudanito001@gmail.com>
2023-03-16 02:41:10 +0100

    Light upload to see if backend runs

--- dist/dbConnection.js
+++ dist/dbConnection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@localhost:5433/kcg_crm', { logging: false });
+const sequelize = new sequelize_1.Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@kcg-crm-database.cer9bnpguytl.us-east-1.rds.amazonaws.com:5432/kcg_crm_database', { logging: false });
 /* const sequelize = new Sequelize('postgres', 'postgres', 'programmer95', {
   host: config.DB_LOCALHOST,
   dialect: 'postgres'
--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,7 +1,7 @@
 import config from './config/config';
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@localhost:5433/kcg_crm', {logging: false})
+const sequelize = new Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@kcg-crm-database.cer9bnpguytl.us-east-1.rds.amazonaws.com:5432/kcg_crm_database', {logging: false})
 
 /* const sequelize = new Sequelize('postgres', 'postgres', 'programmer95', {
   host: config.DB_LOCALHOST,
