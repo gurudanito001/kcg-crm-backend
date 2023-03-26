2dd5c2ce4949de8a55cdcdd726efa598a58ee376
Daniel Nwokocha <gurudanito001@gmail.com>
2023-03-26 22:48:49 +0100

    tried something to fix the pg error on vercel

--- dist/dbConnection.js
+++ dist/dbConnection.js
@@ -1,7 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@kcg-crm-database.cer9bnpguytl.us-east-1.rds.amazonaws.com:5432/kcg_crm_database', { logging: false });
+const pg_1 = __importDefault(require("pg"));
+const sequelize = new sequelize_1.Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@kcg-crm-database.cer9bnpguytl.us-east-1.rds.amazonaws.com:5432/kcg_crm_database', {
+    logging: false,
+    dialectModule: pg_1.default,
+});
 /* const sequelize = new Sequelize('postgres', 'postgres', 'programmer95', {
   host: config.DB_LOCALHOST,
   dialect: 'postgres'
--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,7 +1,13 @@
 import config from './config/config';
 import { Sequelize } from "sequelize";
+import pg from 'pg';
 
-const sequelize = new Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@kcg-crm-database.cer9bnpguytl.us-east-1.rds.amazonaws.com:5432/kcg_crm_database', {logging: false})
+
+
+const sequelize = new Sequelize('postgres://daniel:fsWxKmrDfufmqESvC3TH@kcg-crm-database.cer9bnpguytl.us-east-1.rds.amazonaws.com:5432/kcg_crm_database', {
+  logging: false,
+  dialectModule: pg,
+})
 
 /* const sequelize = new Sequelize('postgres', 'postgres', 'programmer95', {
   host: config.DB_LOCALHOST,
